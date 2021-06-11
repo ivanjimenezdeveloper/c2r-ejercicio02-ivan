a4a01808d7b0db0eafe3da6d6aa772e3359cd3de
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-11 19:39:58 +0200

    Imagen Senyor finalizada

--- src/components/ImagenSenyor.js
+++ src/components/ImagenSenyor.js
@@ -1,8 +1,20 @@
-export const ImagenSenyor = () => {
+export const ImagenSenyor = (props) => {
+  const { foto, nombre } = props;
+  const nombrePalabras = nombre.split(" ");
+  const getInicial = () =>
+    nombrePalabras[0].length < 3
+      ? nombrePalabras[1].charAt(0)
+      : nombrePalabras[0].charAt(0);
+
   return (
     <div className="avatar col">
-      <img src="" className="img-fluid rounded-circle" width="214" alt="" />
-      <span className="inicial text-center rounded-circle"></span>
+      <img
+        src={`../img/${foto}`}
+        className="img-fluid rounded-circle"
+        width="214"
+        alt={nombre}
+      />
+      <span className="inicial text-center rounded-circle">{getInicial()}</span>
     </div>
   );
 };
--- src/components/Senyor.js
+++ src/components/Senyor.js
@@ -3,11 +3,14 @@ import { InformacionSenyor } from "./InformacionSenyor";
 import { IconoCheck } from "./IconoCheck";
 
 export const Senyor = (props) => {
+  const { senyor } = props;
+  const { foto, nombre } = senyor;
+
   return (
     <>
       <article className="senyor col-8 offset-2 mb-4">
         <div className="row">
-          <ImagenSenyor />
+          <ImagenSenyor foto={foto} nombre={nombre} />
           <InformacionSenyor />
         </div>
         <IconoCheck />
